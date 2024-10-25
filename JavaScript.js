9ca993e4db10d988a99a871053784804bc145e37
Pedro Augusto <maracujafrut@gmail.com>
2024-10-25 10:52:51 -0300

    MUDANÇAS

---
+++ JavaScript.js
@@ -0,0 +1,15 @@
+//CARROSSEL SETAS
+
+$(document).ready(function() {
+    // Adiciona a classe active ao clicar nas setas
+    $('.carousel-control-prev').click(function() {
+        $('.carousel-control-prev-icon').addClass('active'); // Seta anterior
+        $('.carousel-control-next-icon').removeClass('active'); // Remove da seta seguinte
+    });
+
+    $('.carousel-control-next').click(function() {
+        $('.carousel-control-next-icon').addClass('active'); // Seta seguinte
+        $('.carousel-control-prev-icon').removeClass('active'); // Remove da seta anterior
+    });
+});
+
